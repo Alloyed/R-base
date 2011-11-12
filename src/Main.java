e6eca72a2e0ed72e8019efe7f2dd3ee009635818
Kyle McLamb <kjmclamb@gmail.com>
2011-11-11 20:11:15 -0500

    Main menu! settings(sort of)!

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,11 @@
  * 
  * WITH COMMENTS
  *  
+ *  TODO:Work out classes for robots, levels, etc. fix "toward cursor" mode, get friction and multiple bodies
+ *  PROBABLE PROBLEMS: No "window" in gui, cannot control mouse acceleration
  */
+
+import java.util.ArrayList;
 
 import controlP5.*; //gooey
 import processing.core.*; //graphix
@@ -14,20 +18,34 @@ import org.jbox2d.dynamics.*;
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.*;
 
-/*
- * TODO: Work out classes for robots, etc.
- */
+
 public class Main extends PApplet {
 	private static final long serialVersionUID = 1L;
 	World physics;
-	Body pc; //this should be nicer
 	ControlP5 gooey;
-	boolean ROTATE_FORCE = true; //does W point up, or to the cursor?
-	boolean UP_PRESSED, LEFT_PRESSED, RIGHT_PRESSED, DOWN_PRESSED; //This would be a good example of 'user data' for bodies
+	boolean ROTATE_FORCE = false; //does W point up, or to the cursor?
+	boolean menuOn = true; //Are we looking at the menu?
+	//This stuff would be a good example of 'user data' for bodies
+	boolean UP_PRESSED, LEFT_PRESSED, RIGHT_PRESSED, DOWN_PRESSED; 
+	Body pc;
 	float speed = 5; //vroom vroom
+	ArrayList<ControllerInterface> mainMenu;
+	
 	Vec2 angtoVec(float ang, float mag)
 	{
 		return new Vec2(mag*cos(ang),mag*sin(ang));
+	}
+	
+	public void quit()
+	{
+		exit();
+	}
+	
+	public void resume()
+	{
+		gooey.controller("resume").setLabel("resume");
+		menuOn = !menuOn;
+		doMenu();
 	}
 	
 	public void setup()
@@ -36,6 +54,7 @@ public class Main extends PApplet {
 		size(800,600);
 		frameRate(60);
 		background(0);
+		smooth();
 		//Physix stuf
 		physics = new World(new Vec2(0,0),false);
 		
@@ -47,28 +66,28 @@ public class Main extends PApplet {
 		FixtureDef fd = new FixtureDef();
 		fd.shape = s;
 		fd.density = .5f;
-		fd.friction = .9f; //Dat coefficient
+		fd.friction = .4f; //Dat coefficient
+		
 		pc = physics.createBody(d);
 		pc.createFixture(fd);
+		
 		//Gooey Stuf
 		gooey = new ControlP5(this);
-		gooey.addButton("quit"); //calls method /w name quit
-		
-	}
-	
-	public void quit()
-	{
-		exit();
+		gooey.load("controlP5.xml");//See that for the gooey options. controller names correspond with method names
+		mainMenu = new ArrayList<ControllerInterface>();
+		for (ControllerInterface c : gooey.getControllerList())
+			mainMenu.add(c);
+		gooey.controller("resume").setLabel("start");
 	}
 	
 	public void draw()
-	{
-		Vec2 move = new Vec2(0,0);
+	{	
 		//Physix
+		Vec2 move = new Vec2(0,0);
 		Vec2 dir = new Vec2(mouseX,mouseY).sub(pc.getWorldCenter().mul(64));
 		float ang = atan2(dir.y,dir.x);
 		pc.setTransform(pc.getWorldCenter(),ang);
-		
+
 		if (UP_PRESSED)
 			move.addLocal(0, -speed);
 		if (LEFT_PRESSED)
@@ -81,10 +100,21 @@ public class Main extends PApplet {
 			move.set(move.x*cos(ang)-move.y*sin(ang),move.x*sin(ang)+move.y*cos(ang));
 		}
 		pc.applyForce(move, new Vec2());
+		
 		physics.step(1f/60f, 8, 3);
-		physics.clearForces(); //The book said to so I will
-		//Drawing
-		background(0);
+		physics.clearForces();
+	
+		if (!menuOn) { //GAME!
+			worldDraw();
+		} else { //MENU!
+			background(150);
+		}
+	}
+	
+	//This will be much more important later, I imagine.
+	void worldDraw()
+	{
+		background(20);
 		Vec2 v = pc.getPosition();
 		pushMatrix();
 		fill(255);
@@ -94,7 +124,17 @@ public class Main extends PApplet {
 		fill(100);
 		rect(0,-16,32,32);
 		popMatrix();
-		gooey.draw();
+	}
+	
+	//This is called when the menu is turned on or off
+	void doMenu() 
+	{
+		for (ControllerInterface c: mainMenu) {
+			if (menuOn)
+				c.show();
+			else
+				c.hide();
+		}
 	}
 	
 	public void keyPressed()
@@ -107,11 +147,16 @@ public class Main extends PApplet {
 			DOWN_PRESSED = true;
 		else if (key == 'd')
 			RIGHT_PRESSED = true;
-			
 	}
 	
 	public void keyReleased()
 	{
+		if (key == TAB) {
+			menuOn = !menuOn;
+			doMenu();
+		} else if (key == 'o')
+			gooey.save();
+		else
 		if (key == 'w')
 			UP_PRESSED = false;
 		else if (key == 'a')
@@ -122,4 +167,8 @@ public class Main extends PApplet {
 			RIGHT_PRESSED = false;
 	}
 	
+	public static void main(String[] args)
+	{
+		 PApplet.main(new String[] { "--present", "Main" });
+	}
 }
