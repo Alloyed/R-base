cac8c1fd519fc73de0ac9d7a1fad033a6187dec2
audiobahn404 <audiobahn404@gmail.com>
2011-11-12 15:48:24 -0500

    Repackaging
    Apparently the last commit didn't include the server package. Let's try
    it again.

---
+++ src/server/Server.java
@@ -0,0 +1,12 @@
+package server;
+
+/*
+ * All this thing does is wait for new clients.
+ * Then ServerThread takes over for each one.
+ */
+
+public class Server {
+	public static void main(String[] args) {
+		
+	}
+}
