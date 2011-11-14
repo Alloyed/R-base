9fe14ddfc3c648b5f24a68df480930acda511d15
audiobahn404 <audiobahn404@gmail.com>
2011-11-13 22:02:46 -0500

    Event enum
    This is going to have all of the possible KeyEvents the user can use to
    shorten error checking elsewhere.

---
+++ src/misc/Event.java
@@ -0,0 +1,14 @@
+package misc;
+
+import java.awt.event.KeyEvent;
+
+public enum Event {
+	VK_UP,
+	VK_DOWN,
+	VK_LEFT,
+	VK_RIGHT,
+	KEY_PRESSED,
+	KEY_RELEASED;
+	
+	/* TODO: write interfacing stuff.
+}
