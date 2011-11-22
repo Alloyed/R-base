94eac36b4808f26f7e3ce0da24bc046e71c6e792
audiobahn404 <audiobahn404@gmail.com>
2011-11-22 14:09:26 -0500

    The Skins Class didn't add. Fixed now. Yeah, fixed it.

---
+++ src/client/Skin.java
@@ -0,0 +1,17 @@
+package client;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class Skin {
+	public HashMap<String,Sprite> sprites;
+	
+	public Skin(Runner p, Settings s) {
+		sprites = new HashMap<String,Sprite>();
+		File dir = new File("data/images/"+s.SKIN_FOLDER+"/");
+		
+		if(dir.exists())
+			for (File f : dir.listFiles())
+				sprites.put(f.getName(), new Sprite(p, f.toString()));
+	}
+}
